73534615b03e6b52f10f2155ccc8312e875759d7
AlexIova <iovaalexandru37@gmail.com>
2023-04-13 18:17:26 +0200

    Conf file + connection

--- WordleClient/WordleClientMain.java
+++ WordleClient/WordleClientMain.java
@@ -1,12 +1,77 @@
 package WordleClient;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Properties;
+import java.util.Scanner;
+
 /**
  * WordleClientMain
  */
 public class WordleClientMain {
 
-    /** The entry point of application. Just prints hello world. */
+    private static String pathProp = "./WordleClient/client.properties";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Lorem ipsum qui ci sono le tue opzioni:\n\t1. Register \n\t2. Login\n\t3. Logout\n\t4. Play\n\t5. sendMeStatistics \n\t6. share \n\t 7. showMeSharing \n\t 8. exit");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        scanner.close();
+        Properties properties = initProp(pathProp);
+        String SERVER_ADDRESS = properties.getProperty("SERVER_ADDRESS");
+        int SERVER_PORT = Integer.parseInt(properties.getProperty("SERVER_PORT"));
+        try (Socket clientSocket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+            switch (option) {
+                case 1:
+                    System.out.println("Register");
+                    break;
+                case 2:
+                    System.out.println("Login");
+                    break;
+                case 3:
+                    System.out.println("Logout");
+                    break;
+                case 4:
+                    System.out.println("Play");
+                    break;
+                case 5:
+                    System.out.println("sendMeStatistics");
+                    break;
+                case 6:
+                    System.out.println("share");
+                    break;
+                case 7:
+                    System.out.println("showMeSharing");
+                    break;
+                case 8:
+                    System.out.println("exit");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            System.err.println("Error connecting to server: " + e.getMessage());
+        }
     }
+
+    private static Properties initProp(String pathProp) {
+        Properties properties = new Properties();
+        try {
+            FileInputStream input = new FileInputStream(pathProp);
+            properties.load(input);
+            input.close();
+        } catch (IOException e) {
+            System.err.println("Error loading properties: " + e.getMessage());
+        }
+        return properties;
+    }
+
 }
--- WordleServer/WordleServerMain.java
+++ WordleServer/WordleServerMain.java
@@ -1,12 +1,68 @@
 package WordleServer;
 
+
+import java.net.*;
+import java.io.*;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * WordleServerMain
  */
 public class WordleServerMain {
 
+    private static String pathProp = "./WordleServer/server.properties";
+
     public static void main(String[] args) {
-        // Print hello world
-        System.out.println("Hello World!");
+        Properties properties = inProperties(pathProp);
+        int SERVER_PORT = Integer.parseInt(properties.getProperty("SERVER_PORT"));
+        int THREAD_POOL_SIZE = Integer.parseInt(properties.getProperty("THREAD_POOL_SIZE"));
+        ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            System.out.println("TCP Server started on port " + SERVER_PORT);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connection accepted from " + clientSocket.getInetAddress().getHostAddress());
+                threadPool.execute(new ClientHandler(clientSocket));
+            }
+        } catch (IOException e) {
+            System.err.println("Error starting TCP Server: " + e.getMessage());
+        }
+    }
+
+    private static Properties inProperties(String pathProp) {
+        Properties properties = new Properties();
+        try {
+            FileInputStream input = new FileInputStream(pathProp);
+            properties.load(input);
+            input.close();
+        } catch (IOException e) {
+            System.err.println("Error loading properties: " + e.getMessage());
+        }
+        return properties;        
+    }
+
+    public static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("Received message from " + clientSocket.getInetAddress().getHostAddress() + ": " + message);
+                    writer.println("Echo: " + message);
+                }
+            } catch (IOException e) {
+                System.err.println("Error handling client request: " + e.getMessage());
+            }
+        }
     }
 }
