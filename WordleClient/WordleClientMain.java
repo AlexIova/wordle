53ca3fd4edf654ea1feedb2297cca92963c3c1e7
AlexIova <iovaalexandru37@gmail.com>
2023-04-13 16:49:27 +0200

    First files

---
+++ WordleClient/WordleClientMain.java
@@ -0,0 +1,12 @@
+package WordleClient;
+
+/**
+ * WordleClientMain
+ */
+public class WordleClientMain {
+
+    /** The entry point of application. Just prints hello world. */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ WordleServer/WordleServerMain.java
@@ -0,0 +1,12 @@
+package WordleServer;
+
+/**
+ * WordleServerMain
+ */
+public class WordleServerMain {
+
+    public static void main(String[] args) {
+        // Print hello world
+        System.out.println("Hello World!");
+    }
+}
