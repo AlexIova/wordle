0d60935db8829385890734ae6ff4f5c9d3d51529
AlexIova <iovaalexandru37@gmail.com>
2023-04-17 16:33:58 +0200

    Infinite cycle command

--- WordleClient/InstructionsClient.java
+++ WordleClient/InstructionsClient.java
@@ -3,109 +3,103 @@ import java.util.*;
 
 public class InstructionsClient {
     
-    public static void handleRegistration(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream){
+    public static void handleRegistration(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream, Scanner scanner){
         System.out.println("You are now trying to register. \nPlease enter your username: ");
-        try (Scanner scanner = new Scanner(System.in)) {
-            String username = scanner.nextLine();
-            System.out.println("Please enter your password: ");
-            String password = scanner.nextLine();
-            RegisterMsg registerMsg = new RegisterMsg(username, password);
-            try {
-                objectOutputStream.writeObject(registerMsg);
-                System.out.println(registerMsg);
-                System.out.println("Registration request sent");
-                Messaggio reply = (Messaggio) objectInputStream.readObject();
-                if (reply.getType() == MessageType.STATUS) {
-                    StatusMsg statusMsg = (StatusMsg) reply;
-                    switch (statusMsg.getCode()) {
-                        case 0:
-                            System.out.println("Registration successfull");
-                            break;
-                        case 1:
-                            System.out.println("Username already exists");
-                            break;
-                        case 2:
-                            System.out.println("Empty password field");
-                            break;
-                        default:
-                            System.out.println("Unknown error code");
-                            break;
-                    }
-                } else {
-                    throw new WrongMessageException("Invalid reply");
+        String username = scanner.nextLine();
+        System.out.println("Please enter your password: ");
+        String password = scanner.nextLine();
+        RegisterMsg registerMsg = new RegisterMsg(username, password);
+        try {
+            objectOutputStream.writeObject(registerMsg);
+            System.out.println(registerMsg);
+            System.out.println("Registration request sent");
+            Messaggio reply = (Messaggio) objectInputStream.readObject();
+            if (reply.getType() == MessageType.STATUS) {
+                StatusMsg statusMsg = (StatusMsg) reply;
+                switch (statusMsg.getCode()) {
+                    case 0:
+                        System.out.println("Registration successfull");
+                        break;
+                    case 1:
+                        System.out.println("Username already exists");
+                        break;
+                    case 2:
+                        System.out.println("Empty password field");
+                        break;
+                    default:
+                        System.out.println("Unknown error code");
+                        break;
                 }
-            } catch (ClassNotFoundException | IOException | WrongMessageException e) { e.printStackTrace(); }
-        }
+                return;
+            } else {
+                throw new WrongMessageException("Invalid reply");
+            }
+        } catch (ClassNotFoundException | IOException | WrongMessageException e) { e.printStackTrace(); }
     }
 
 
-    public static void handleLogin(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream){
+    public static void handleLogin(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream, Scanner scanner){
         System.out.println("You are now trying to login. \nPlease enter your username: ");
-        try (Scanner scanner = new Scanner(System.in)) {
-            String username = scanner.nextLine();
-            System.out.println("Please enter your password: ");
-            String password = scanner.nextLine();
-            LoginMsg loginMsg = new LoginMsg(username, password);
-            try {
-                objectOutputStream.writeObject(loginMsg);
-                System.out.println(loginMsg);
-                System.out.println("Login request sent");
-                Messaggio reply = (Messaggio) objectInputStream.readObject();
-                if (reply.getType() == MessageType.STATUS) {
-                    StatusMsg statusMsg = (StatusMsg) reply;
-                    switch (statusMsg.getCode()) {
-                        case 0:
-                            System.out.println("Login successfull");
-                            break;
-                        case 1:
-                            System.out.println("Already logged in");
-                            break;
-                        case 2:
-                            System.out.println("Wrong password");
-                            break;
-                        case 3:
-                            System.out.println("Username not found");
-                            break;
-                        default:
-                            System.out.println("Unknown error code");
-                            break;
-                    }
-                } else {
-                    throw new WrongMessageException("Invalid reply");
+        String username = scanner.nextLine();
+        System.out.println("Please enter your password: ");
+        String password = scanner.nextLine();
+        LoginMsg loginMsg = new LoginMsg(username, password);
+        try {
+            objectOutputStream.writeObject(loginMsg);
+            System.out.println(loginMsg);
+            System.out.println("Login request sent");
+            Messaggio reply = (Messaggio) objectInputStream.readObject();
+            if (reply.getType() == MessageType.STATUS) {
+                StatusMsg statusMsg = (StatusMsg) reply;
+                switch (statusMsg.getCode()) {
+                    case 0:
+                        System.out.println("Login successfull");
+                        break;
+                    case 1:
+                        System.out.println("Already logged in");
+                        break;
+                    case 2:
+                        System.out.println("Wrong password");
+                        break;
+                    case 3:
+                        System.out.println("Username not found");
+                        break;
+                    default:
+                        System.out.println("Unknown error code");
+                        break;
                 }
-            } catch (ClassNotFoundException | IOException | WrongMessageException e) { e.printStackTrace(); }
-        }
+            } else {
+                throw new WrongMessageException("Invalid reply");
+            }
+        } catch (ClassNotFoundException | IOException | WrongMessageException e) { e.printStackTrace(); }
     }
 
 
-    public static void handleLogout(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream){
+    public static void handleLogout(ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream, Scanner scanner){
         System.out.println("You are now trying to logout. \nPlease enter your username: ");
-        try (Scanner scanner = new Scanner(System.in)) {
-            String username = scanner.nextLine();
-            System.out.println("Please enter your password: ");
-            LogoutMsg logoutMsg = new LogoutMsg(username);
-            try {
-                objectOutputStream.writeObject(logoutMsg);
-                System.out.println(logoutMsg);
-                System.out.println("Logout request sent");
-                Messaggio reply = (Messaggio) objectInputStream.readObject();
-                if (reply.getType() == MessageType.STATUS) {
-                    StatusMsg statusMsg = (StatusMsg) reply;
-                    switch (statusMsg.getCode()) {
-                        case 0:
-                            System.out.println("Logout successfull");
-                            break;
-                        case 1:
-                            System.out.println("Not logged in");
-                            break; 
-                        default:
-                            System.out.println("Unknown error code");
-                            break;
-                    }
-                } else {
-                    throw new WrongMessageException("Invalid reply");
+        String username = scanner.nextLine();
+        LogoutMsg logoutMsg = new LogoutMsg(username);
+        try {
+            objectOutputStream.writeObject(logoutMsg);
+            System.out.println(logoutMsg);
+            System.out.println("Logout request sent");
+            Messaggio reply = (Messaggio) objectInputStream.readObject();
+            if (reply.getType() == MessageType.STATUS) {
+                StatusMsg statusMsg = (StatusMsg) reply;
+                switch (statusMsg.getCode()) {
+                    case 0:
+                        System.out.println("Logout successfull");
+                        break;
+                    case 1:
+                        System.out.println("Not logged in");
+                        break; 
+                    default:
+                        System.out.println("Unknown error code");
+                        break;
                 }
-            } catch (ClassNotFoundException | IOException | WrongMessageException e) { e.printStackTrace(); }
-        }
+            } else {
+                throw new WrongMessageException("Invalid reply");
+            }
+        } catch (ClassNotFoundException | IOException | WrongMessageException e) { e.printStackTrace(); }
     }
 }
