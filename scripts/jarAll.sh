5b8a491f3203b9eb2440cd71c5ed13aa9c872318
AlexIova <iovaalexandru37@gmail.com>
2023-04-27 15:54:55 +0200

    now compiled jar files

---
+++ scripts/jarAll.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+mkdir ../jars
+
+# Server jar
+cd ../WordleServer
+make
+rm manifest.txt         # Remove previous manifest.txt
+echo 'Main-Class: WordleServerMain' >> manifest.txt
+echo 'Class-Path: ../gson-2.10.1.jar' >> manifest.txt
+jar cvfm WordleServer.jar manifest.txt *.class ../gson-2.10.1.jar
+mkdir ../jars/server
+cp WordleServer.jar ../jars/server/
+cp server.properties ../jars/server/
+make clean
+
+# Client jar
+cd ../WordleClient
+make
+rm manifest.txt         # Remove previous manifest.txt
+echo 'Main-Class: WordleClientMain' >> manifest.txt
+jar cvfm WordleClient.jar manifest.txt *.class
+mkdir ../jars/client
+cp WordleClient.jar ../jars/client/
+cp client.properties ../jars/client/
+make clean
