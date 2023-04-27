351e918b00c1d417a45028cb5218e124da654290
AlexIova <iovaalexandru37@gmail.com>
2023-04-27 15:57:50 +0200

    jars now moved not copied

---
+++ scripts/jarAll.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+mkdir ../jars
+
+# Server jar
+cd ../WordleServer
+make
+rm manifest.txt         # Remove previous manifest.txt
+echo 'Main-Class: WordleServerMain' >> manifest.txt
+echo 'Class-Path: ../gson-2.10.1.jar' >> manifest.txt
+jar cvfm WordleServer.jar manifest.txt *.class ../gson-2.10.1.jar
+mkdir ../jars/server
+mv WordleServer.jar ../jars/server/
+mv server.properties ../jars/server/
+make clean
+
+# Client jar
+cd ../WordleClient
+make
+rm manifest.txt         # Remove previous manifest.txt
+echo 'Main-Class: WordleClientMain' >> manifest.txt
+jar cvfm WordleClient.jar manifest.txt *.class
+mkdir ../jars/client
+mv WordleClient.jar ../jars/client/
+mv client.properties ../jars/client/
+make clean
